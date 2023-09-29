ebd1fe2fc805e3537d402601396a156b88fb0997
alokrai0607 <saelalok001@gmail.com>
2023-09-29 13:35:20 +0530

    Vendor API

---
+++ CloudApi/src/main/java/com/cloud/Controller/CloudVendorApiService.java
@@ -0,0 +1,62 @@
+package com.cloud.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cloud.Models.CloudVender;
+
+@RestController
+@RequestMapping("/cloudVendor")
+public class CloudVendorApiService {
+
+	CloudVender cloudVender;
+
+	// http://localhost:8080/cloudVendor/07 (Get)
+	@GetMapping("{vendorId}")
+	public CloudVender getCloudVenderdetails(String vendorId) {
+
+		return cloudVender;
+
+	}
+
+	// http://localhost:8080/cloudVendor (Post)
+
+	/*
+	 * { "vendorId": "07", "vendorName": "Alok", "vendorAddress": "Bhagwanpur",
+	 * "vendorPhoneNumber": "90444414580" }
+	 */
+
+	@PostMapping
+	public String createCloudVenderDetails(@RequestBody CloudVender cloudVender) {
+
+		this.cloudVender = cloudVender;
+
+		return "Cloud vender created Successfully";
+
+	}
+     
+	//localhost:8080/cloudVendor
+	/*{
+	    "vendorId": "07",
+	    "vendorName": "Alok Rai Shivam",
+	    "vendorAddress": "Bhagwanpur",
+	    "vendorPhoneNumber": "90444414580"
+	}*/
+	
+	@PutMapping
+ 	public String UpdateCloudVenderDetails(@RequestBody CloudVender cloudVender) {
+
+		this.cloudVender = cloudVender;
+
+		return "Cloud vender updated Successfully";
+
+	}
+	
+	
+	
+
+}
